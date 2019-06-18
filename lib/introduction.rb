8f8777b2f93b1c24437e47cf32cd4ce025953be0
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-18 02:57:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(dan, ember.js)
+def introduction_with_language(dan, emberjs)
   puts "Hi, my name is #{Dan} and I am learning to program in #{Ember}."
 end
